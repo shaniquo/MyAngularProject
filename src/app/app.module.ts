dd34a6e77bdd1ef28bd94fe33b9513c49e11a545
shaniquo <61515994+shaniquo@users.noreply.github.com>
2022-01-06 21:45:10 -0500

    created my first angular project
    update to make a flower shop web app

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';  // Add this
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule   // Add here
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
